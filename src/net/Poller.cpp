a745ed877583e071c09d2254d673db664ae1dfa3
poilk <poilkxx@163.com>
2022-07-10 09:29:25 +0800

    fix: poller update channel failed

--- src/net/Poller.cpp
+++ src/net/Poller.cpp
@@ -12,32 +12,28 @@
 
 namespace penduo {
 
-
-Poller::Poller(EventLoop *loop) : owner_loop_(loop){
-
-}
-
-Poller::~Poller() {
+Poller::Poller(EventLoop *loop) : owner_loop_(loop) {
 
 }
+
+Poller::~Poller() = default;
 
 void Poller::poll(int timeout_ms, ChannelList &active_channels) {
   int num_events = ::poll(pollfds_.data(), pollfds_.size(), timeout_ms);
-  if(num_events > 0){
+  if (num_events > 0) {
     LOG_TRACE << "eventloop " << owner_loop_ << " " << num_events << " events happend";
     fill_active_channels(num_events, active_channels);
-  } else if (num_events == 0){
+  } else if (num_events == 0) {
     LOG_TRACE << "eventloop " << owner_loop_ << " nothing happened";
-  } else{
+  } else {
     LOG_SYS_ERROR << "Poller::poll()";
   }
-  return;
 }
 
 void Poller::fill_active_channels(int num_events, ChannelList &active_channels) {
-  for(auto const &pfd : pollfds_){
+  for (auto const &pfd : pollfds_) {
     //todo whate does revents < 0 mean?
-    if(pfd.revents > 0){
+    if (pfd.revents > 0) {
       num_events--;
       assert(channels_.count(pfd.fd) != 0);
       Channel *channel = channels_.at(pfd.fd);
@@ -45,7 +41,7 @@ void Poller::fill_active_channels(int num_events, ChannelList &active_channels) 
       channel->set_revents(pfd.revents);
       active_channels.push_back(channel);
     }
-    if(!num_events){
+    if (!num_events) {
       break;
     }
   }
@@ -55,24 +51,24 @@ void Poller::update_channel(Channel *channel) {
   assert_in_loop_thread();
   assert(channel != nullptr);
   LOG_TRACE << "fd = " << channel->fd() << " events = " << channel->events();
-  if (channel->get_pollfds_index() < 0){
+  if (channel->get_pollfds_index() < 0) {
     // a new one add to pollfds
     assert(channels_.count(channel->fd()) == 0);
     struct pollfd pfd{channel->fd(), static_cast<short>(channel->events()), 0};
     pollfds_.push_back(pfd);
     const int idx = static_cast<int>(pollfds_.size()) - 1;
     channel->set_pollfds_index(idx);
-    channels_.insert({ channel->fd(), channel});
-  } else{
+    channels_.insert({channel->fd(), channel});
+  } else {
     // update existing one
     assert(channels_.count(channel->fd()) != 0);
     assert(channels_.at(channel->fd()) == channel);
     const int idx = channel->get_pollfds_index();
     assert(0 <= idx && idx < static_cast<int>(pollfds_.size()));
-    auto pfd = pollfds_[idx];
+    auto &pfd = pollfds_[idx];
     assert(pfd.fd == channel->fd() || pfd.fd == ~channel->fd());
     pfd = {channel->fd(), static_cast<short>(channel->events()), 0};
-    if(channel->is_none_event()){
+    if (channel->is_none_event()) {
       pfd.fd = ~channel->fd();
     }
   }
